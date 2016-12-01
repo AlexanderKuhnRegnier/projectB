f596cca3052a8ba7a91450a761731a6adcd975fa
Alexander Kuhn-Regnier <ahf.kuhnregnier@gmail.com>
2016-12-01 18:18:31 +0000

    Added new module for the static AMR sizing

---
+++ AMR_system.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 01 18:11:34 2016
+
+@author: Alexander Kuhn-Regnier
+"""
+from __future__ import print_function
+import numpy as np
+import matplotlib.pyplot as plt
+from numba import jit
+from scipy import sparse
+# np.set_printoptions(threshold=np.inf)
+
